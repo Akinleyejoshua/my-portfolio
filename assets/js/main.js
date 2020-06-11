b32965169e93c2e89c202700b87bde70a6f1ef45
Joshua <akinleyejoshua.dev@gmail.com>
2020-06-11 23:19:51 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,45 @@
+var header = $("header");
+var navBrand = $(".nav-brand");
+var navLinks = $(".nav-link");
+var menuBtn = $('.navbar-toggler div')
+var nav = document.querySelector('.nav-bar');
+var navToggler = document.querySelectorAll('.navbar-toggler div')
+
 const toggleNav = () => {
-    var nav = document.querySelector('.nav-bar');
-    var navToggler = document.querySelectorAll('.navbar-toggler div')
+
 
     if (nav.style.height) {
         nav.style.height = null;
         for (let index = 0; index < navToggler.length; index++) {
             navToggler[index].classList.remove("active")
         }
+        header.removeClass("scroll");
+        navLinks.removeClass("scroll");
+        navBrand.removeClass("scroll");
     } else {
         nav.style.height = nav.scrollHeight + "px";
         for (let index = 0; index < navToggler.length; index++) {
             navToggler[index].className += " active"
         }
+        header.addClass("scroll");
+        navLinks.addClass("scroll");
+        navBrand.addClass("scroll");
+        navToggler.className += " active"
     }
 }
+
+
+$(window).on('scroll', () => {
+    if (window.scrollY >= 10) {
+        header.addClass("scroll");
+        navLinks.addClass("scroll");
+        menuBtn.addClass("scroll")
+        navBrand.addClass("scroll");
+    } else {
+        header.removeClass("scroll");
+        navLinks.removeClass("scroll");
+        menuBtn.removeClass("scroll")
+        navBrand.removeClass("scroll");
+    }
+})
 
